a3b46cb89ab1aa47208f7b6f07e966e92cdcceb3
panwaratul <panwaratul82@gmail.com>
2016-10-11 11:54:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
- res.send(createtamplate(articleone));
+ res.send(createtemplate(articleone));
 });
 app.get('/article-two',function(req,res){
  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));   
