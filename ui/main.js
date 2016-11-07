3f58d86ede780d79f45cd28accff003ffa563541
panwaratul <panwaratul82@gmail.com>
2016-11-07 08:44:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='New value';
 
 //move the image
-var img=document.getElementById;
+var img=document.getElementById('madi');
 img.onclick=function(){
     img.style.marginLeft='100px';
 };
