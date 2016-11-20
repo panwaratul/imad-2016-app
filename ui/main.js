23766db193c8c78d9e4398c00547dae5d347c7d6
panwaratul <panwaratul82@gmail.com>
2016-11-20 17:34:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,6 +53,6 @@ submit.onclick = function() {
     //Make a request
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET', 'http://neha-panwar.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.open('GET', 'http://panwaratul.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
